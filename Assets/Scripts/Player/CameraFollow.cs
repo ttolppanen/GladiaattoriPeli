0fbb6d0944c2faba37943ceb812145492abdf375
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-08-01 16:03:37 +0300

    Perus liikkuminen, kattominen

---
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    GameObject player;
+    Vector3 difference;
+
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+        difference = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + difference;
+	}
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	public float speed;
+    public float maxSpeed;
+    Rigidbody rb;
+
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }
+        if (rb.velocity.magnitude > maxSpeed)
+        {
+            rb.velocity = rb.velocity.normalized * maxSpeed;
+        }
+    }
+}
