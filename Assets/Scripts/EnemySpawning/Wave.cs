e4549a31ee46b3170a5c4c25efec5413c1d0dc6b
Taneli Tolppanen <taneli.tolppanen@gmail.com>
2018-08-02 11:38:43 +0300

    Spawneri

---
+++ Assets/Scripts/EnemySpawning/Wave.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnableObject
+{
+    public GameObject enemy;
+    public int amount;
+
+    public SpawnableObject(GameObject enemy, int amount)
+    {
+        this.enemy = enemy;
+        this.amount = amount;
+    }
+}
