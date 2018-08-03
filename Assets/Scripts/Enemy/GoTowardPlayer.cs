180567033955456d62c513a04add0cf4f81e8200
Oskari <oskari266@hotmail.com>
2018-08-03 21:55:43 +0300

    scripti

---
+++ Assets/Scripts/Enemy/GoTowardPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoTowardPlayer : MonoBehaviour {
+    GameObject player;
+    Rigidbody boi;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        boi = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 directionToPlayer = player.transform.position - transform.position;
+        if(player != null)
+        {
+            float angleToPlayer = Mathf.Atan2(directionToPlayer.z, directionToPlayer.x);
+            Vector3 enemyRot = transform.rotation.eulerAngles;
+            enemyRot.y = -angleToPlayer * Mathf.Rad2Deg+90;
+            transform.rotation = Quaternion.Euler(enemyRot);
+            boi.AddForce(directionToPlayer);
+        }
+	}
+}
